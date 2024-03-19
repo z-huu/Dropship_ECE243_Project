500191d943480fc54cbdb17bc110744de5f3d0f8
z-huu <zach_1003@hotmail.com>
2024-03-19 12:07:24 -0400

    more functions for gameplay loop

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define RESOLUTION_X 320
 #define RESOLUTION_Y 240
@@ -13,6 +14,8 @@ typedef struct spaceship
     int orientationX;
     int orientationY;
     int health;
+    bool canShoot;
+    bool wantShoot;
 } ship;
 
 typedef struct projectile
@@ -21,8 +24,7 @@ typedef struct projectile
     int y;
     int dx;
     int dy;
-    int orientationX; // orientation might not be required for bullets
-    int orientationY;
+    // circle shaped bullets; no orientation needed
 
 } bullet;
 
@@ -37,17 +39,22 @@ int shipRight[][];
 int shipUpRight[][];
 
 int titleScreen[][]; // buffer to hold title screen image
-
+int mapLayout[][][];   //buffer to hold the map layout aka obstacles
+                       // index one for which map layout we're using,
+                       // second & third for obstacle coordinates
 int gameState = 0;
-
+int currentMap = 0;     // by default, use the first map layout
 #define TITLE_SCREEN 0
 #define IN_GAME 1
 #define WINNER_A 2
 #define WINNER_B 3
 
 void eraseShips(ship *shipA, ship *shipB);
-
-ship shipA, shipB;
+void eraseOldBullets( bullet *bulletArray);
+void handleCollisions(bullet* bulletArray, int**** mapLayout, ship* shipA, ship* shipB, int currentMap);
+void checkWinner(ship *shipA, ship* shipB);
+ship *shipA; ship *shipB;   //ship pointers
+bullet bulletArray[];   //array to keep track of all bullet objects
 
 int main()
 {
@@ -61,11 +68,14 @@ int main()
     { // gameplay while loop
         // hi
 
-        eraseShips(&shipA, &shipB); // erase previous spaceships
-        // erase previous bullets
+        eraseShips(shipA, shipB); // erase previous spaceships
+        eraseOldBullets(bulletArray); // erase previous bullets
+
         // handle bullet collisions (obstacles, or spaceship) + bullet deletion
+        handleCollisions(bulletArray, mapLayout, shipA, shipB, currentMap);
         // update spaceship health
-        // check for winner; if health = 0, clear screen, switch states
+
+        checkWinner(shipA, shipB); // check for winner; if health = 0, clear screen, switch states
         // grab user input (do they wanna move, do they wanna shoot)
         // if user can, and wants to shoot, spawn a bullet
         // queue shooting sound
