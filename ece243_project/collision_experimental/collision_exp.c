bf7a9831fa8901e6bb90ba7c513a842e07914b5d
Abhigokul Kupendiran <138940977+AbhigokulK@users.noreply.github.com>
2024-03-25 14:05:25 -0400

    Added Keyboard Controls and Border Collisions * Has interesting trail effects

--- ece243_project/collision_experimental/collision_exp.c
+++ ece243_project/collision_experimental/collision_exp.c
@@ -1,5 +1,40 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
+/*
+*** *** ***
+MAIN TO DO LIST:
+
+1: figure out how to implement keyboard with interrupts
+    - the main structure should be pretty similar, just with setting current input via the interrupt, and handling input during the interrupt
+
+2: try to make it so the player controls are held in separate player keyboard objects?
+    - options are:
+        A: use a single keyboard object, which will handle the entire keyboard space
+            - in this case, the division between players will occur at the midpoint of the arrays
+                (ie lets say we have 4 inputs per player. the bindings and directions will be 2*4 = 8 indices, first 4 for P1, last 4 for P2)
+            - leaning towards this since it makes handling single separate keyboards MUCH easier
+            - also, if we manage to have multiple keyboards, it becomes trivial to expand for that
+                - simply use a different keyboard_ptr value for the other keyboard address
+        B: modify player_keyboard object, as well as how inputs are handled
+            - would probably need a significant restructuring of how we poll for keyboard inputs
+            - leaning away from this since im not sure how this would fit if i were to go with interrupts as well...
+
+3: figure out how to make a simple box for our space ship
+    - essentially, on a frame (1/60 s), we move the ship by speed*dx + speed*dy in x and y directions respectively
+    - would be good to use hex display numbers?
+    - maybe figure out how to display numbers on VGA? (may be overkill for just testing though)
+    - will be used to test collisions and movement via keyboard
+    - probably will be done in a separate project/C file to keep things a bit tidy
+
+IDEAS FOR PHYSICS:
+- maybe have a physics struct, which has points to a large amount of physics based stuff
+    - for example, has pointers to the space ships (in an array), array containing all the projectiles, and contains the collision mask as well
+
+*** *** ***
+*/
+
+// VGA DECLARATION
 volatile int pixel_buffer_start;  // global variable
 short int Buffer1[240][512];	  // 240 rows, 512 (320 + padding) columns
 short int Buffer2[240][512];
@@ -16,6 +51,8 @@ const int N = 8;
 const int speed = 3;
 
 void clear_screen();
+
+void plot_pixel(int x, int y, short int line_color);
 
 void draw_line(int x0, int y0, int x1, int x2, short int line_colour);
 
@@ -51,8 +88,152 @@ struct hitbox {
 const int hitbox_total = 1;
 short int collision_frame[240][320];
 
-int main(void) {
-	volatile int* pixel_ctrl_ptr = (int*)0xFF203020;
+// TIMER DECLARATION
+struct timer{
+    volatile unsigned int status;
+    volatile unsigned int control;
+    volatile unsigned int start_low_value;
+    volatile unsigned int start_high_value;
+    volatile unsigned int count_low;
+    volatile unsigned int count_high;
+};
+/* KEYBOARD DECLARATION
+PS/2 Port Description (port 1 start: 0xFF200100, port 2 start: 0xFF200108)
+for both, it is the data register then the control register
+256-byte FIFO
+address: Data Register:
+bits 0-7 -> data (readable and writeable!)
+bit 15 -> R_VALID (if 1, then we get the head of the data FIFO)
+bits 16-31 -> R_AVAIL (displays number of entries in the FIFO, reading decreases it by 1)
+
+(address + 4): Control Register
+bit 0: RE (enable interrupts for when R_AVAIL > 0, ie when FIFO has data)
+bit 8: RI (when interrupt is pending, RI = 1. reset by emptying the FIFO)
+bit  10: CE (shows if an error has occurred when sending a command TO the PS/2)
+*/
+
+#define inputs_per_player (int) 8
+#define BR_CODE (char) 0xF0
+#define MULTI_BYTE_CODE (char) 0xE0
+
+struct ps2{
+    volatile unsigned char data; // 8 bits of data
+    volatile unsigned char R_VALID;
+    volatile unsigned short int R_AVAIL;
+    volatile unsigned int control;
+};
+
+// stores the byte code AND if it is multiple bytes long
+// multi_byte key codes will ALWAYS start with E0, then whatever code it is!
+typedef struct key_code{
+    char byte_code;
+    bool multi_byte;
+} key_code;
+
+// container for the controls a player may have
+typedef struct player_keyboard{
+    bool is_breaking;
+    bool is_multi_byte;
+    key_code bindings[inputs_per_player];
+    bool directions[inputs_per_player];
+    struct ps2 *keyboard_ptr;
+} player_keyboard;
+
+int check_key_direction(player_keyboard *keyboard, char current_input){
+    for(int k = 0; k < inputs_per_player; k++){
+        // iterate through the key codes, check if the current input matches
+        // if it does, check if that key is a multi-byte, and if we are actively looking for an multi byte as well
+        if(current_input == keyboard->bindings[k].byte_code){
+            // we are touching a key we care about! return its corresponding slot/direction
+            if(keyboard->bindings[k].multi_byte == keyboard->is_multi_byte){
+                return k;
+            }
+        }
+    }
+    // we didnt touch a key we care about
+    return -1;
+}
+
+void handle_Keyboard(player_keyboard *keyboard, char *current_input){
+    if((keyboard->keyboard_ptr)->R_AVAIL){
+        // there is data available
+        *current_input = keyboard->keyboard_ptr->data;
+        
+        int key_state;
+        if(*current_input == BR_CODE){
+            // breaking key start
+            keyboard->is_breaking = true;
+        }else if(keyboard->is_breaking){
+            // breaking key end point, actually "let go" of key
+            key_state = check_key_direction(keyboard, *current_input);
+            if(key_state != -1){
+                keyboard->directions[key_state] = 0;
+            }
+            keyboard->is_breaking = false;
+            keyboard->is_multi_byte = false;
+        }else if(*current_input == MULTI_BYTE_CODE){
+            // multi key input start!
+            keyboard->is_multi_byte = true;
+        }else{
+            // normal input, check if it is an input we care about
+            key_state = check_key_direction(keyboard, *current_input);
+            if(key_state != -1){
+                keyboard->directions[key_state] = true;
+            }
+            // we are done looking for a multi-byte entry, we should already be getting one
+            keyboard->is_multi_byte = false;
+            keyboard->is_breaking = false;
+        }
+    }
+}
+
+struct timer *const timer_ptr = ((struct timer*)0xFF202000);
+struct ps2 *const ps2_ptr1 = ((struct ps2*) 0xFF200100);
+struct ps2 *const ps2_ptr2 = ((struct ps2*) 0xFF200108);
+volatile int *switch_ptr = (int*) (0xFF200040);
+volatile int *LEDs = (int*) 0xff200000;
+volatile int *HEX_0 = (int*) 0xFF200020;
+volatile int *HEX_5 = (int*) 0xFF200030;
+
+typedef struct spaceship
+{
+    int x;
+    int y;
+    int old_x;
+    int old_y;
+    int dx;
+    int dy;
+    int orientationX;
+    int orientationY;
+    int health;
+    bool canShoot;
+    bool wantShoot;
+} ship;
+
+#define ship_size 5
+#define frame_rate 60
+#define timer_frequency 100000000
+#define ship_speed 3
+
+void handle_ship_physics(ship *player_ship){
+    // check collisions of ship, stop movement in that direction if it will collide
+    if(player_ship->x + ship_speed * player_ship->dx > x_min && 
+    player_ship->x + ship_speed * player_ship->dx < x_max - ship_size){
+        // within bounds, move in X direction
+        player_ship->x = player_ship->x + ship_speed * player_ship->dx;
+    }
+    
+    if(player_ship->y + ship_speed * player_ship->dy > y_min &&
+    player_ship->y + ship_speed * player_ship->dy < y_max - ship_size){
+        // within bounds, move in Y direction
+        player_ship->y = player_ship->y + ship_speed * player_ship->dy;
+    }
+    
+}
+
+int main(void){
+    // VGA SET UP
+    	volatile int* pixel_ctrl_ptr = (int*)0xFF203020;
 	// declare other variables(not shown)
 	// initialize location and direction of rectangles(not shown)
 
@@ -78,10 +259,10 @@ int main(void) {
 	struct hitbox colliders[hitbox_total];
 
 	// randomize locations of hitboxes and their sizes
-	colliders[0].x1 = 50;
-	colliders[0].y1 = 50;
-	colliders[0].x2 = 150;
-	colliders[0].y2 = 150;
+	colliders[0].x1 = 200;
+	colliders[0].y1 = 200;
+	colliders[0].x2 = 220;
+	colliders[0].y2 = 220;
 	/*for (int k = 0; k < hitbox_total; k++) {
 		colliders[k].x1 = ((rand() % x_max/2));
 		colliders[k].y1 = ((rand() % y_max/2));
@@ -108,96 +289,105 @@ int main(void) {
 		}
 	}
 
-	int x_box[N];
-	int y_box[N];
-	int old_x[N];
-	int old_y[N];
-	int dx[N];
-	int dy[N];
-	short int boxColour[N];
+    int LED_Out = 0;
 
-	// initialize variables (positions, directions, colors)
-	initializePosition(x_box, N, 320);
-	initializePosition(y_box, N, 240);
-	initializeDirection(dx, N);
-	initializeDirection(dy, N);
-	for (int k = 0; k < N; k++) {
-		boxColour[k] = color_list[rand() % 10];
-	}
-	while (1) {
-		// wait for vsync and update buffer positions
+    // PLAYER 1 KEYBOARD SET UP
+    const key_code player1_movement_bindings[inputs_per_player];
+    bool p1_directions[inputs_per_player];
+
+    player_keyboard p1_keyboard = {
+        .is_breaking = 0,
+        .is_multi_byte = 0,
+        .bindings = {
+            {0x1d, false}, // W
+            {0x1b, false}, // S
+            {0x1c, false}, // A
+            {0x23, false}, // D
+            {0x75, true}, // UP ARROW
+            {0x72, true}, // DOWN ARROW
+            {0x6b, true}, // LEFT ARROW
+            {0x74, true} // RIGHT ARROW
+        },
+        .directions = p1_directions,
+        .keyboard_ptr = ps2_ptr1
+    };
+    
+    p1_keyboard.keyboard_ptr->data = 0xF4;
+    p1_keyboard.keyboard_ptr->data = 0xFF;
+
+    char current_input;
+
+    // TIMER SET UP
+    timer_ptr->start_low_value = (timer_frequency/frame_rate);
+    timer_ptr->start_high_value = ((timer_frequency/frame_rate) >> 16);
+    timer_ptr->control = 0b0110;
+
+    ship player1_ship;
+    player1_ship.x = 50;
+    player1_ship.y = 50;
+
+    while(1){
+        LED_Out = 0;
+        handle_Keyboard(&p1_keyboard, &current_input);
+        
+        //Physics Step
+        /*
+        if(timer_ptr->status & 0b1){
+            // Physics Timer Finished, handle physics now
+            timer_ptr->status = 0b0;
+            LED_Out = LED_Out ^ 0b100000000;
+            player1_ship.dy = p1_directions[0] - p1_directions[1];
+            player1_ship.dx = p1_directions[2] - p1_directions[3];
+            player1_ship.orientationY = p1_directions[4] - p1_directions[5];
+            player1_ship.orientationX = p1_directions[6] - p1_directions[7];
+            handle_ship_physics(&player1_ship);
+        }
+        */
+
+        player1_ship.dy = p1_keyboard.directions[1] - p1_keyboard.directions[0];
+        player1_ship.dx = p1_keyboard.directions[3] - p1_keyboard.directions[2];
+        handle_ship_physics(&player1_ship);
+        
+        // RENDER STEP
+        // wait for vsync and update buffer positions
 		wait_for_vsync();
 		pixel_buffer_start = *(pixel_ctrl_ptr + 1);
 
 		// erase old boxes
-		draw_frame(old_x, old_y, NULL, N);
-
-		// move boxes and update old positions
-		for (int k = 0; k < N; k++) {
-			old_x[k] = x_box[k];
-			old_y[k] = y_box[k];
-		}
-		handle_physics(x_box, y_box, dx, dy, N, speed, collision_frame);
+		draw_box(player1_ship.old_x, player1_ship.old_y, ship_size, ship_size, 0);
+        
+        draw_box(colliders[0].x1, colliders[0].y1, 
+                colliders[0].x2 - colliders[0].x1,
+                colliders[0].y2 - colliders[0].y1,
+                0);
 
 		// draw hitboxes
-		for (int k = 0; k < hitbox_total; k++) {
+        for (int k = 0; k < hitbox_total; k++) {
 			draw_box(colliders[k].x1, colliders[k].y1,
 					 colliders[k].x2 - colliders[k].x1,
 					 colliders[k].y2 - colliders[k].y1, color_list[1]);
 		}
+        
+
+        player1_ship.old_x = player1_ship.x;
+        player1_ship.old_y = player1_ship.y;
 
 		// draw new boxes at shifted positions
-		draw_frame(x_box, y_box, boxColour, N);
-	}
+        //int colour_index = abs(player1_ship.orientationX) + abs((player1_ship.orientationY << 1)) + 1;
+		draw_box(player1_ship.x, player1_ship.y, ship_size, ship_size, color_list[2]);
+
+
+        // output data onto LEDS
+        for(int k = 0; k < inputs_per_player; k++){
+            if(p1_keyboard.directions[k]){
+                LED_Out += 0x1 << k;
+            }
+        }
+        *LEDs = LED_Out;
+    }
 }
 
-void handle_physics(int* x_box, int* y_box, int* dx, int* dy, int N, int speed,
-					short int** collision_frame) {
-	// for each box...
-	for (int k = 0; k < N; k++) {
-		// boundary checks
-		if (x_box[k] + (speed * dx[k]) <= x_min ||
-			x_box[k] + (speed * dx[k]) >= x_max) {
-			// redirect dx
-			dx[k] *= -1;
-		}
-		if (y_box[k] + (speed * dy[k]) <= y_min ||
-			y_box[k] + (speed * dy[k]) >= y_max) {
-			// redirect dy
-			dy[k] *= -1;
-		}
-
-		// TO DO: ADD COLLISION CHECKS
-		if (x_box[k] + (speed * dx[k]))
-
-			// move point
-			x_box[k] += dx[k] * speed;
-		y_box[k] += dy[k] * speed;
-	}
-}
-
-// to erase, set boxColour to null
-void draw_frame(int* x_box, int* y_box, short int* boxColour, int N) {
-	for (int k = 0; k < N - 1; k++) {
-		if (boxColour) {
-			draw_line(x_box[k], y_box[k], x_box[k + 1], y_box[k + 1],
-					  boxColour[k]);
-			draw_box(x_box[k], y_box[k], box_size, box_size, boxColour[k]);
-		} else if (boxColour == NULL) {
-			draw_line(x_box[k], y_box[k], x_box[k + 1], y_box[k + 1], 0);
-			draw_box(x_box[k], y_box[k], box_size, box_size, 0);
-		}
-	}
-	// draw final line
-	if (boxColour) {
-		draw_line(x_box[0], y_box[0], x_box[N - 1], y_box[N - 1], boxColour[N]);
-		draw_box(x_box[N - 1], y_box[N - 1], box_size, box_size, boxColour[N]);
-	} else if (boxColour == NULL) {
-		draw_line(x_box[0], y_box[0], x_box[N - 1], y_box[N - 1], 0);
-		draw_box(x_box[N - 1], y_box[N - 1], box_size, box_size, 0);
-	}
-}
-
+// VGA FUNCTION DEFINITIONS
 void draw_box(int x, int y, int x_size, int y_size, short int color) {
 	for (int a = x; a < x + x_size + 1; a++) {
 		for (int b = y; b < y + y_size + 1; b++) {
@@ -215,18 +405,6 @@ void wait_for_vsync() {
 		status = *(pixel_ctrl_ptr + 3);
 	}
 	// should exit when vsync occurs!
-}
-
-void initializePosition(int* arr, int size, int range) {
-	for (int k = 0; k < size; k++) {
-		arr[k] = ((rand() % range));
-	}
-}
-
-void initializeDirection(int* arr, int size) {
-	for (int k = 0; k < size; k++) {
-		arr[k] = ((rand() % 2) * 2) - 1;
-	}
 }
 
 int poll_status(volatile int* status_reg) {
@@ -289,7 +467,7 @@ void swap(int* a, int* b) {
 void plot_pixel(int x, int y, short int line_color) {
 	volatile short int* one_pixel_address;
 
-	one_pixel_address = pixel_buffer_start + (y << 10) + (x << 1);
+	one_pixel_address = (int) pixel_buffer_start + (y << 10) + (x << 1);
 
 	*one_pixel_address = line_color;
 }
